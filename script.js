bba646755d1ac37d893bb298fdfd5714a60e5cfe
Samsreefuture <samsreefuture@gmail.com>
2026-07-18 03:21:31 +0530

    Added mobile menu

--- script.js
+++ script.js
@@ -61,4 +61,31 @@ const typing = document.querySelector(".typing-text");
 
 if (typing) {
     typeEffect();
+}// Mobile Menu
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navMenu = document.querySelector("nav ul");
+
+if (menuToggle) {
+    menuToggle.addEventListener("click", () => {
+        navMenu.classList.toggle("active");
+    });
+}// Mobile Menu
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navMenu = document.querySelector("nav ul");
+
+if (menuToggle) {
+    menuToggle.addEventListener("click", () => {
+        navMenu.classList.toggle("active");
+    });
+}// Mobile Menu
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navMenu = document.querySelector("nav ul");
+
+if (menuToggle) {
+    menuToggle.addEventListener("click", () => {
+        navMenu.classList.toggle("active");
+    });
 }
